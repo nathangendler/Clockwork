49e6f66a199188c5166ff0e044835eb9a0a5378c
nathangendler <106348682+nathangendler@users.noreply.github.com>
2026-02-07 19:27:30 -0500

    Proxy /logout in dev server

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:8080',
       '/auth': 'http://localhost:8080',
+      '/logout': 'http://localhost:8080',
     },
   },
 })
